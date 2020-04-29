ea32ae2ae60b7b1b693312b748f95df2093e3e76
yikwing <hubeieasy@gmail.com>
2020-04-29 15:19:07 +0800

    add aidl

---
+++ app/src/main/aidl/com/yikwing/aidlservice/IYiPay.aidl
@@ -0,0 +1,10 @@
+// IYiPay.aidl
+package com.yikwing.aidlservice;
+
+// Declare any non-default types here with import statements
+
+interface IYiPay {
+
+     String getMoney(String username,String password);
+
+}
--- app/src/main/java/com/yikwing/myapplication/TwoActivity.kt
+++ app/src/main/java/com/yikwing/myapplication/TwoActivity.kt
@@ -1,20 +1,32 @@
 package com.yikwing.myapplication
 
 import android.Manifest
+import android.content.ComponentName
+import android.content.Context
+import android.content.Intent
+import android.content.ServiceConnection
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.IBinder
 import android.widget.Toast
 import androidx.lifecycle.Observer
 import com.alibaba.android.arouter.facade.annotation.Route
+import com.yikwing.aidlservice.IYiPay
 import com.yikwing.livedatapermission.LivePermissions
 import com.yikwing.livedatapermission.PermissionResult
+import kotlinx.android.synthetic.main.activity_two.*
 
 
 @Route(path = "/app/two")
 class TwoActivity : AppCompatActivity() {
+
+    var iYiPay: IYiPay? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_two)
+
+        registerService()
 
         LivePermissions(this).request(
             Manifest.permission.WRITE_EXTERNAL_STORAGE,
@@ -41,5 +53,36 @@ class TwoActivity : AppCompatActivity() {
             }
         })
 
+        btnPay.setOnClickListener {
+//
+//            if (iYiPay == null) {
+//                registerService()
+//            }
+
+            val money =
+                iYiPay?.getMoney(username.text.toString().trim(), password.text.toString().trim())
+                    ?: "请先安装易支付"
+
+            Toast.makeText(this, money, Toast.LENGTH_SHORT).show()
+
+        }
+
+    }
+
+
+    private fun registerService() {
+        val intent = Intent("con.giao.ge.ipay")
+        intent.setPackage("com.yikwing.aidlservice")
+
+        bindService(intent, object : ServiceConnection {
+            override fun onServiceDisconnected(name: ComponentName?) {
+                iYiPay = null
+            }
+
+            override fun onServiceConnected(name: ComponentName?, service: IBinder?) {
+                iYiPay = IYiPay.Stub.asInterface(service)
+            }
+
+        }, Context.BIND_AUTO_CREATE)
     }
 }
